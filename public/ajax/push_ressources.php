b506b4a02fa91b75d3c1152b48ff303424cea08b
Francis-Ouellet <franc.ouelle93@gmail.com>
2014-02-17 22:36:59 -0500

    Gestion des messages et Suppression d'un groupe de ressources

--- public/ajax/push_ressources.php
+++ public/ajax/push_ressources.php
@@ -24,101 +24,159 @@
 		
 			//Connexion à la BD.
 			$bdd = new PDO("mysql:host=$dbHote;dbname=$dbNom", $dbUser, $dbPwd, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+			
+			if($_POST['type'] == "addGroup"){
+			addGroup($bdd);
+			}
+			else if($_POST['type'] == "modifGroup" ){
+				modifGroup($bdd);
+			}
+			else if($_POST['type'] == "suppGroup" ){
+				suppGroup($bdd);
+			}
+			else if($_POST['type'] == "changeActif"){
+				changeActif($bdd);
+			}
+			else if($_POST['type'] == "ajoutBlocs"){
+				ajoutBlocs($bdd);
+			}
 		}
 		catch(PDOException $e){
 			echo "Erreur lors de la recherche des groupes.";
 		}
-		
-		if($_POST['type'] == "addGroup"){
-			addGroup($bdd);
-		}
-		else if($_POST['type'] == "modifGroup" ){
-			modifGroup($bdd);
-		}
-		else if($_POST['type'] == "changeActif"){
-			changeActif($bdd);
-		}
-		else if($_POST['type'] == "ajoutBlocs"){
-			ajoutBlocs($bdd);
-		}
 	}
 	
 	function addGroup($bdd){
-		$nom = $_POST['nom'];
-		$desc = $_POST['description'];
-		
-		$sql = 'Call saveBlocRessource(?,?)';
-		$req = $bdd->prepare($sql);
-		$req->bindParam(1,$nom);
-		$req->bindParam(2,$desc);
-		
-		$req->execute();
-		
-		$resultat = $req->fetch();
-		
-		$json = array('id' => $resultat['idBlocRessource'], 'nom' => $resultat['nomBloc'], 'description' => $resultat['description'], 'actif' => $resultat['used']);
-		
-		echo json_encode($json);
+
+		if (isset($_POST['nom']) && trim($_POST['nom']) != ""){
+			
+			if (isset($_POST['description']) && trim($_POST['description']) != ""){
+				$nom = $_POST['nom'];
+				$desc = $_POST['description'];
+				
+				$sql = 'Call saveBlocRessource(?,?)';
+				$req = $bdd->prepare($sql);
+				$req->bindParam(1,$nom);
+				$req->bindParam(2,$desc);
+				
+				$req->execute();
+				
+				$resultat = $req->fetch();
+				
+				$json = array('id' => $resultat['idBlocRessource'], 'nom' => $resultat['nomBloc'], 'description' => $resultat['description'], 'actif' => $resultat['used']);
+				
+				echo json_encode($json);
+			}
+			else{
+				echo json_encode(array("message" => "Un bloc d'horaire doit avoir une description."));
+			}
+		}
+		else{
+			echo json_encode(array("message" => "Un bloc d'horaire doit avoir un nom."));
+		}
 	}
 	
 	function modifGroup($bdd){
-		$id = $_POST['id'];
-		$nom = $_POST['nom'];
-		$desc = $_POST['description'];
-		
-		$sql = 'Call editBlocRessource(?,?,?)';
-		$req = $bdd->prepare($sql);
-		$req->bindParam(1,$id);
-		$req->bindParam(2,$nom);
-		$req->bindParam(3,$desc);
-		
-		$req->execute();
-		
-		$resultat = $req->fetch();
-		
-		$json = array('id' => $resultat['idBlocRessource'], 'nom' => $resultat['nomBloc'], 'description' => $resultat['description'], 'actif' => $resultat['used']);
-		
-		echo json_encode($json);
+
+		if (isset($_POST['id'],$_POST['nom']) && trim($_POST['nom']) != ""){
+			if(isset($_POST['description']) && trim($_POST['description']) != ""){
+				$id = $_POST['id'];
+				$nom = $_POST['nom'];
+				$desc = $_POST['description'];
+				
+				$sql = 'Call editBlocRessource(?,?,?)';
+				$req = $bdd->prepare($sql);
+				$req->bindParam(1,$id);
+				$req->bindParam(2,$nom);
+				$req->bindParam(3,$desc);
+				
+				$req->execute();
+				
+				$resultat = $req->fetch();
+				
+				$json = array('id' => $resultat['idBlocRessource'], 'nom' => $resultat['nomBloc'], 'description' => $resultat['description'], 'actif' => $resultat['used']);
+				
+				echo json_encode($json);
+			}
+			else{
+				echo json_encode(array("message" => "Un groupe de ressources doit avoir une description."));
+			}
+		}
+		else{
+			echo json_encode(array("message" => "Un groupe de ressources doit avoir un nom."));
+		}
+
+	}
+
+	function suppGroup($bdd){
+		if(isset($_POST['id'])){
+			$id = $_POST['id'];
+			$sql = 'Call SupprimeBlocRessource(?)';
+			$req = $bdd->prepare($sql);
+			$req->bindParam(1,$id);
+
+			$req->execute();
+
+			echo json_encode(array("message" => "Le groupe a été supprimé avec succès."));
+		}
+		else {
+			echo json_encode(array("message" => "Une erreur est survenue."));
+		}
 	}
 	
 	function changeActif($bdd){
-		$id = $_POST['noGroupe'];
-		
-		$sql = 'Call setUsedMere(?)';
-		$req = $bdd->prepare($sql);
-		$req->bindParam(1,$id);
-		
-		$req->execute();
+		if (isset($_POST['noGroupe'])){
+
+			$id = $_POST['noGroupe'];
+			
+			$sql = 'Call setUsedMere(?)';
+			$req = $bdd->prepare($sql);
+			$req->bindParam(1,$id);
+			
+			$req->execute();
+		}
+		else{
+			echo json_encode(array("message" => "Une erreur est survenue."));
+		}
 	}
 	
 	function ajoutBlocs($bdd){
-		$id = $_POST['groupId'];
-		
-		$sql = 'Call deleteResourcesByGroup(?)';
-		$req = $bdd->prepare($sql);
-		$req->bindParam(1,$id);
-		$req->execute();
-		
-		foreach($_POST['lesBlocs'] as $bloc){
-			$jour = $bloc['jour'];
-			$debut = $bloc['heureDebut'];
-			$fin = $bloc['heureFin'];
-			$chaussures = $bloc['nbChaussures'];
-			$vetements = $bloc['nbVetements']; 
-			$caissiers = $bloc['nbCaisses'];
-			
-			$sql = 'Call addRessource(?,?,?,?,?,?,?)';
-			$req = $bdd->prepare($sql);
-			$req->bindParam(1,$id);
-			$req->bindParam(2,$jour);
-			$req->bindParam(3,$debut);
-			$req->bindParam(4,$fin);
-			$req->bindParam(5,$chaussures);
-			$req->bindParam(6,$vetements);
-			$req->bindParam(7,$caissiers);
-			$req->execute();
+
+		if(isset($_POST['groupId'])){
+			if(isset($_POST['lesBlocs'])){
+				$id = $_POST['groupId'];
+				$sql = 'Call deleteResourcesByGroup(?)';
+				$req = $bdd->prepare($sql);
+				$req->bindParam(1,$id);
+				$req->execute();
+				
+				foreach($_POST['lesBlocs'] as $bloc){
+					$jour = $bloc['jour'];
+					$debut = $bloc['heureDebut'];
+					$fin = $bloc['heureFin'];
+					$chaussures = $bloc['nbChaussures'];
+					$vetements = $bloc['nbVetements']; 
+					$caissiers = $bloc['nbCaisses'];
+					
+					$sql = 'Call addRessource(?,?,?,?,?,?,?)';
+					$req = $bdd->prepare($sql);
+					$req->bindParam(1,$id);
+					$req->bindParam(2,$jour);
+					$req->bindParam(3,$debut);
+					$req->bindParam(4,$fin);
+					$req->bindParam(5,$chaussures);
+					$req->bindParam(6,$vetements);
+					$req->bindParam(7,$caissiers);
+					$req->execute();
+				}
+			}
+			else{
+				echo json_encode(array("message" => "Il faut ajouter des blocs de ressource pour pouvoir enregistrer des ressources.", "type" => "success"));
+			}
 		}
-		
+		else{
+			echo json_encode(array("message" => "Il faut d'abord créer un groupe de ressources pour enregistrer des blocs de ressource.", "type" => "error"));
+		}		
 	}
 	
 ?>
