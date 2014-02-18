7ee4eec330aa133bd5a76f96e18dcda5f28759c3
Marc-Antoine Bouchard M <marcantoine.bouchardm@gmail.com>
2014-02-17 23:31:58 -0800

    Correctif telephone

--- app/controllers/HoraireController.php
+++ app/controllers/HoraireController.php
@@ -5,12 +5,13 @@ class HoraireController extends BaseController {
 	public function genere() {
 		$creationHoraire = HoraireModel::creeHoraire();
 
-		if ($creationHoraire) {
-			return Redirect::route('horaire')->withSuccess("L'horaire à bien été généré.");
-		} else if ($creationHoraire == "no.ressource") {
+		if ($creationHoraire == "no.ressource") {
 			return Redirect::route('horaire')->withFail("Vous devez ajouter des ressources.");
+		} else if (!$creationHoraire) {
+			return Redirect::route('horaire')->withFail("Il manque d'employé pour combler toute les resssources. Une horaire à tout de même été généré.");
 		}
-		return Redirect::route('horaire')->withFail("Il manque d'employé pour combler toute les resssources. Une horaire à tout de même été généré.");
+
+		return Redirect::route('horaire')->withSuccess("L'horaire à bien été généré.");
 	}
 
 	public function notif() {
