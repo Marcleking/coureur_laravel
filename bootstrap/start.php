5b360e9892b29f7d7a00a475b60eb04f5ae8088c
charles <charles.delmaire@gmail.com>
2014-02-19 20:09:25 -0500

    ajout de jeux essai

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Marc-Antoine', 'Samuel-PC'),
+	'local' => array('Marc-Antoine', 'Samuel-PC', 'Charles-PC'),
 
 ));
 
