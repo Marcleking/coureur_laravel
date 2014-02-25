e704972e1f1f1bd2e6c7b3aadac9ab29fa3b36d5
charles <charles.delmaire@gmail.com>
2014-02-24 21:44:34 -0500

    ajout de la vue remplacement

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -35,4 +35,76 @@
 			@endif
 		</div>
 	</div>
+	<div class="medium-12 columns">
+		<h3>Remplacements</h3>
+		<div id="princ" class="panel">
+		<div>
+	</div>
+	
+	<script type="text/javascript">
+		window.addEventListener('load',init,false);
+
+		function init(){
+			importerHoraire();
+				
+				
+		}
+		
+		
+		function importerHoraire() {
+			$.ajax({
+					url:"{{ URL::asset('ajax/fetch_remplacement.php')}}",
+					type:"POST",
+					dataType:"json",
+					error:function (){},
+					success:function(horaire){
+						console.log(horaire);
+						
+						for(var i = 0; i< horaire.length; i++) {
+							var divPrin = document.getElementById('princ');
+							var div = document.createElement('div');
+							var divHrs = document.createElement('div');
+							var divCheck = document.createElement('div');
+							
+							divHrs.innerHTML = convertJour(horaire[i]["jour"]) + "<br />" + horaire[i]["debut"] + " - " + horaire[i]["fin"] ;
+							divHrs.setAttribute("class","inline");
+							div.appendChild(divHrs);
+							div.appendChild(divCheck);
+							div.setAttribute("class","plage");
+							divPrin.appendChild(div);
+						}
+					}
+				});
+		}
+		
+		function convertJour(no) {
+			var jour;
+			switch (parseInt(no))
+			{
+			case 0:
+			  jour="Lundi";
+			  break;
+			case 1:
+			  jour= "Mardi";
+			  break;
+			case 2:
+			  jour= "Mercredi";
+			  break;
+			case 3:
+			  jour= "Jeudi";
+			  break;
+			case 4:
+			  jour= "Vendredi";
+			  break;
+			case 5:
+			  jour= "Samedi";
+			  break;
+			case 6:
+			  jour= "Dimanche";
+			  break;
+			}
+			console.log(no);
+			return jour;
+		}
+	</script>
 @stop
---
+++ public/ajax/fetch_remplacement.php
@@ -0,0 +1,44 @@
+<?php
+	header('Content-Type: application/json');
+	
+	// Retourne du contenu en format JSON.
+	header("Content-type: text/html; charset=utf-8");
+
+	// Force l'expiration immédiate de la page au niveau du navigateur Web; elle n'est pas conservée en cache.
+	header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");
+	header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+	header("Pragma: no-cache");
+
+	$dbHote = "localhost";
+	$dbNom = "coureur_nordique";
+	$dbUser = "user_coureur";
+	$dbPwd = "qweqwe";
+
+	try{
+		//Options pour la gestion et messages d'erreur.
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+	
+		//Connexion à la BD.
+		$bdd = new PDO("mysql:host=$dbHote;dbname=$dbNom", $dbUser, $dbPwd, array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+	
+			$sql = 'Call afficherRemplacement()';
+			$req = $bdd->prepare($sql);
+			$req->execute();
+
+			$resultat = $req->fetchAll();
+			$json = array();
+
+			foreach ($resultat as $plage) {
+				$json[] = array("jour" => $plage["jour"], "type" => $plage["typeTravail"], 
+					"debut" => $plage["heureDebut"], "fin" => $plage["heureFin"]);
+			}
+
+			echo json_encode($json);
+		
+
+	}
+	catch(PDOException $e){
+		echo "Erreur lors de la recherche des groupes.";
+	}
+
+?>
