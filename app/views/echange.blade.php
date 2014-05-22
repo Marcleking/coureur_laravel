230932aea4896fdccb77afc5cbaed1365c745013
Francis-Ouellet <franc.ouelle93@gmail.com>
2014-05-21 21:55:16 -0400

    Modification gestion de la facon d'envoyer une demande d'échange

--- app/views/echange.blade.php
+++ app/views/echange.blade.php
@@ -38,47 +38,111 @@
 							var divPrin = document.getElementById('divPrinc');
 							var div = document.createElement('div');
 							var divHrs = document.createElement('div');
+							var divType = document.createElement('div');
 							var divCheck = document.createElement('div');
 							var check = document.createElement('input');
-							check.setAttribute("type","checkbox");
-							check.value = horaire[i]["id"];
-							check.addEventListener("change", changeReplacementStatus, false);
+							check.setAttribute("type","button");
+							check.id = horaire[i]["remplacement"] + horaire[i]["id"];
+							check.className = "button small";
+							check.addEventListener("click", askForConfirmation, false);
+							check.style.margin = "10px";
 
 							if(horaire[i]["remplacement"] != "0"){
-								check.checked = true;
+
+								check.value = "Annuler la demande";
+							}
+							else
+							{
+								check.value = "Demander un remplacement";
 							}
 							
-							divHrs.innerHTML = convertJour(horaire[i]["jour"]) + "<br />" + horaire[i]["debut"] + " - " + horaire[i]["fin"] ;
-							divCheck.setAttribute("class","right inline");
+							divHrs.innerHTML = convertJour(horaire[i]["jour"]) + "    " + horaire[i]["debut"] + " - " + horaire[i]["fin"];
+							divType.innerHTML = horaire[i]["type"];
+							divType.setAttribute("class", "medium-4 inline");
+							divType.style.margin = "10px";
+							divCheck.setAttribute("class","right");
 							divCheck.appendChild(check);
-							divHrs.setAttribute("class","inline");
+							divHrs.setAttribute("class","medium-4");
+							divHrs.style.margin = "10px";
 							div.appendChild(divHrs);
+							div.appendChild(divType);
 							div.appendChild(divCheck);
-							div.setAttribute("class","plage");
+							div.setAttribute("class","plage row");
 							divPrin.appendChild(div);
 						}
 					}
 				});
 		}
 
-		function changeReplacementStatus(e){
-			var no = e.target.value;
-			var valeur = 0;
-			// Le checkBox est "checked" -> il vient d'être changé, on fait une demande de changement
-			if(e.target.checked)
-			{
-				valeur = 1;
+		function askForConfirmation(e){
+			var conteneur = e.target.parentNode;
+			var boutonConfirmer = document.createElement("input");
+			boutonConfirmer.setAttribute("type","button");
+			boutonConfirmer.className = "button small";
+			boutonConfirmer.value = "Confirmer";
+			boutonConfirmer.id = "confirmer" + e.target.id;
+			boutonConfirmer.style.margin = "10px";
+			boutonConfirmer.addEventListener("click",changeReplacementStatus,false);
+			var boutonAnnuler = document.createElement("input");
+			boutonAnnuler.setAttribute("type","button");
+			boutonAnnuler.className = "button small";
+			boutonAnnuler.value = "Annuler"
+			boutonAnnuler.id = "cancel" + e.target.id;
+			boutonAnnuler.style.margin = "10px";
+			boutonAnnuler.addEventListener("click",annulerDemande,false);
+
+			e.target.style.display = "none";
+			conteneur.appendChild(boutonConfirmer);
+			conteneur.appendChild(boutonAnnuler);
+		}
+
+		function annulerDemande(e){
+			var conteneur = e.target.parentNode;
+			console.log(e.target.id.replace("cancel",""));
+			var boutonAccepter = document.getElementById(e.target.id.replace("cancel",""));
+
+			while(conteneur.hasChildNodes()){
+				conteneur.removeChild(conteneur.lastChild);
 			}
+			boutonAccepter.style.display = "inline";
+			conteneur.appendChild(boutonAccepter);
+		}
+
+		function changeReplacementStatus(e){
+			var id = e.target.id.replace("confirmer","").substr(1); 
+			var valeur = e.target.id.replace("confirmer","").substr(0,1);
 			
+			if(valeur == "0"){
+				valeur = "1";
+			}
+			else{
+				valeur = "0";
+			}
+
 			$.ajax({
 				url:"{{URL::asset('ajax/push_remplacement.php')}}",
 				type:"POST",
-				data:{'id':e.target.value,'valeur':valeur},
+				data:{'id':id,'valeur':valeur},
 				dataType:"json",
 				error:function(){},
 				success:function(resultat){}
 			});
 
+			var conteneur = e.target.parentNode;
+			var bouton = document.getElementById(e.target.id.replace("confirmer",""));
+			while(conteneur.hasChildNodes()){
+				conteneur.removeChild(conteneur.lastChild);
+			}
+
+			if(valeur != "0")
+				bouton.value = "Annuler la demande";
+			else
+				bouton.value = "Demander un remplacement";
+
+			bouton.id = valeur + id;
+
+			bouton.style.display = "inline";
+			conteneur.appendChild(bouton);
 		}
 		
 		function convertJour(no) {
